b4d842814b7e85d25ac8dc658acf4acd14980d11
Alexander <alex.galev@yahoo.com>
2023-02-02 09:31:07 -0700

    passed test

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const p = document.querySelector('#text')
+
+document.addEventListener('DOMContentLoaded', () => {
+	console.warn('DOM has loaded!')
+    p.textContent = 'This is really cool!'
+})
